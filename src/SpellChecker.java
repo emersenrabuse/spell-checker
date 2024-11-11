8ef999947e9ae71c336b3baa8dd9786caf53a362
emersenrabuse <emersen@seas.upenn.edu>
2024-11-11 11:47:23 -0500

    Draft code for SpellChecker and WordRecommender

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -1,14 +1,86 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+
 public class SpellChecker {
 
+    private WordRecommender wordRecommender;
+
     public SpellChecker() {
-        // TODO: You can modify the body of this constructor,
-        // or you can leave it blank. You must keep the signature, however.
+        // use constructor later if needed
     }
 
-    public static void start() {
-        // TODO: You can modify the body of this method,
-        // or you can leave it blank. You must keep the signature, however.
+    public void start() {
+        Scanner scnr = new Scanner(System.in);
+
+        String dictionaryFileName = getValidFileName(Util.DICTIONARY_PROMPT, Util.FILE_OPENING_ERROR, scnr);
+        System.out.printf(Util.DICTIONARY_SUCCESS_NOTIFICATION, dictionaryFileName);
+        wordRecommender = new WordRecommender(dictionaryFileName);
+
+        String spellCheckFileName = getValidFileName(Util.FILENAME_PROMPT, Util.FILE_OPENING_ERROR, scnr);
+        System.out.printf(Util.FILE_SUCCESS_NOTIFICATION, spellCheckFileName);
+
+        spellCheckFile(spellCheckFileName);
     }
 
-    // You can of course write other methods as well.
+    private String getValidFileName(String prompt, String errorMessage, Scanner scnr) {
+        while (true) {
+            System.out.printf(prompt);
+            String fileName = scnr.nextLine();
+            if (new File(fileName).exists()) return fileName;
+            System.out.println(errorMessage);
+        }
+    }
+
+    private void spellCheckFile(String fileName) {
+        try (Scanner fileScanner = new Scanner(new File(fileName));
+             PrintWriter outputWriter = new PrintWriter(fileName.replace(".txt", "chk_txt"))) {
+
+            while (fileScanner.hasNext()) {
+                String word = fileScanner.next();
+                ArrayList<String> suggestions = wordRecommender.getWordSuggestions(word, 2, 0.5, 5);
+
+                if (!suggestions.contains(word)) {
+                    System.out.printf(Util.MISSPELL_NOTIFICATION, word);
+                    System.out.println(Util.FOLLOWING_SUGGESTIONS);
+
+                    for (int i = 0; i < suggestions.size(); i++) {
+                        System.out.printf(Util.SUGGESTION_ENTRY, i + 1, suggestions.get(i));
+                    }
+                    char choice = getReplacementChoice();
+                    if (choice == 'r') {
+                        outputWriter.print(suggestions.get(0) + " ");
+                    } else if (choice == 'a') {
+                        outputWriter.print(word + " ");
+                    } else if (choice == 't') {
+                        outputWriter.print(getManualReplacement() + " ");
+                    }
+                } else {
+                    outputWriter.print(word + " ");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+        }
+    }
+
+    private char getReplacementChoice() {
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            System.out.print(Util.THREE_OPTION_PROMPT);
+            char choice = s.next().charAt(0);
+            if (choice == 'r' || choice == 'a' || choice == 't') {
+                return choice;
+            } else {
+                System.out.printf(Util.INVALID_RESPONSE);
+            }
+        }
+    }
+    private String getManualReplacement(){
+        Scanner s = new Scanner(System.in);
+        System.out.printf(Util.MANUAL_REPLACEMENT_PROMPT);
+        return s.nextLine();
+    }
 }
+
+
--- src/WordRecommender.java
+++ src/WordRecommender.java
@@ -1,19 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class WordRecommender {
 
+    private ArrayList<String> dictionaryWords;
+
     public WordRecommender(String dictionaryFile) {
-        // TODO: change this!
+        dictionaryWords = new ArrayList<>();
+        loadDictionary(dictionaryFile);
+    }
+
+    private void loadDictionary(String dictionaryFile) {
+        try (Scanner scnr = new Scanner (new File(dictionaryFile))) {
+            while (scnr.hasNextLine()) {
+                dictionaryWords.add(scnr.nextLine().trim());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(Util.FILE_OPENING_ERROR);
+        }
     }
 
     public double getSimilarity(String word1, String word2) {
-        // TODO: change this!
-        return 0.0;
+        int commonLetters = 0;
+        for (char c : word1.toCharArray()) {
+            if (word2.indexOf(c) != -1) commonLetters++;
+        }
+        return (double) commonLetters / Math.max(word1.length(), word2.length());
     }
 
     public ArrayList<String> getWordSuggestions(String word, int tolerance, double commonPercent, int topN) {
-        // TODO: change this!
-        return null;
+        ArrayList <String> suggestions = new ArrayList<>();
+        for (String dictWord : dictionaryWords) {
+            double similarity = getSimilarity(word, dictWord);
+            if (similarity >= commonPercent) {
+                suggestions.add(dictWord);
+                if (suggestions.size() >= topN) break;
+            }
+        }
+        return suggestions;
     }
 
 
