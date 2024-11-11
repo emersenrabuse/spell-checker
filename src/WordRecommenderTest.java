846c143957db5c38bb69a374d358f307f0badbca
emersenrabuse <emersen@seas.upenn.edu>
2024-11-11 11:47:35 -0500

    to do list written for WordRecommenderTest

--- src/WordRecommenderTest.java
+++ src/WordRecommenderTest.java
@@ -1,2 +1,16 @@
 public class WordRecommenderTest {
+    // TODO: Write your tests for WordRecommender in WordRecommenderTest.java
+
+        // test if dictionary works correctly
+
+        // test similarity function
+
+        // test getwordsuggestions function
+
+        // test similarities
+
+        // test if dictionary contains a known word
+
+        // test if no suggestion is given for a word with no similarity
+
 }
