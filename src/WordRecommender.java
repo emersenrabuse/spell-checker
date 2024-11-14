a6b493f461ae12461ab9403fe2e05a1ab8ef48a6
emersenrabuse <emersen@seas.upenn.edu>
2024-11-13 19:28:03 -0500

    tolerance tweaks for WordRecommender.java
    finally recognizes "the" as a word! yay!

--- src/WordRecommender.java
+++ src/WordRecommender.java
@@ -1,8 +1,7 @@
-import java.io.File;
+o.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class WordRecommender {
 
@@ -27,25 +26,54 @@ public class WordRecommender {
         return dictionaryWords.contains(word);
     }
 
-    public double getSimilarity(String word1, String word2) {
-        int commonLetters = 0;
-        for (char c : word1.toCharArray()) {
-            if (word2.indexOf(c) != -1) commonLetters++;
+    public int getSimilarity(String word1, String word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        // Initialize the dp array
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1), dp[i - 1][j] + 1), dp[i][j - 1] + 1);
+                }
+            }
         }
-        return (double) commonLetters / Math.max(word1.length(), word2.length());
+
+        return dp[len1][len2];
     }
 
     public ArrayList<String> getWordSuggestions(String word, int tolerance, double commonPercent, int topN) {
-        ArrayList <String> suggestions = new ArrayList<>();
+        ArrayList<String> suggestions = new ArrayList<>();
+
+        // Define max distance to tolerate
+        int maxDistance = 2;  // Allow up to 2 edits
+
+        // Iterate through the dictionary words
         for (String dictWord : dictionaryWords) {
-            double similarity = getSimilarity(word, dictWord);
-            if (similarity >= commonPercent) {
+            // If the word length is drastically different, skip 
+            if (Math.abs(word.length() - dictWord.length()) > 3) {
+                continue;  // Skip words with more than 3 length difference
+            }
+
+            int distance = getSimilarity(word, dictWord);
+
+            double similarity = 1.0 - (double) distance / Math.max(word.length(), dictWord.length());
+
+            if (similarity >= commonPercent && distance <= maxDistance) {
                 suggestions.add(dictWord);
-                if (suggestions.size() >= topN) break;
+            }
+
+            // Stop if we have reached the desired number of suggestions
+            if (suggestions.size() >= topN) {
+                break;
             }
         }
+
         return suggestions;
     }
-
-
 }
